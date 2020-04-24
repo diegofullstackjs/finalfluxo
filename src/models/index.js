d94f1d5d214299be8dd92f73573e9012320495a5
diegofullstackjs <kelvin.fabiana@gmail.com>
2020-04-24 14:17:45 +0100

    Arquivo Principal onde carrega e exporta todos os Models

---
+++ src/models/index.js
@@ -0,0 +1,9 @@
+const CaixaModel = require('./Caixa');
+const CategoriasModel = require('./Categorias');
+const UsuariosModel = require('./Usuario')
+
+module.exports = {
+    Caixa: CaixaModel,
+    Categorias: CategoriasModel,
+    Usuario: UsuariosModel
+}
