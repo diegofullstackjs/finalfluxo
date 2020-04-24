acd837bf3d21a3d070a216ce3859e77b55d1da3b
diegofullstackjs <kelvin.fabiana@gmail.com>
2020-04-24 14:02:55 +0100

    Novo controller de criar categorias

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -1,4 +1,5 @@
 const CaixaModel = require('../models/Caixa');
+const CategoriaModel = require('../models/Categorias')
 module.exports.create_a_box = async (req,res) => {
         const {caixa,description} = req.body;
         await CaixaModel.create({
@@ -11,4 +12,24 @@ module.exports.create_a_box = async (req,res) => {
             console.warn(e)
             return res.status(200).json({message: "Ocorreu um erro ao criar sua caixa"})
         })
+}
+
+module.exports.create_a_categories = async (req,res) => {
+
+      const {name,description} = req.body;
+      const id = req.usuario.user._id;
+
+      await CategoriaModel.create({
+          name,
+          description,
+          user_id: id
+      }).then((resolve) => {
+        return res.status(200).json(resolve)
+      }).catch((e) => {
+        console.warn(e)
+        return res.status(200).json({
+            message: `Ocorreu um erro ao criar a categoria ${name}`
+        })
+    })
+
 }
